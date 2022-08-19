25780208558a5174732fc96872c7f993e5afc0ff
Darvas Istvan <darvasvok@gmail.com>
2022-08-19 16:23:43 +0200

    Installing laravel

---
+++ webpack.mix.js
@@ -0,0 +1,30 @@
+const mix = require('laravel-mix')
+const path = require('path')
+
+mix.webpackConfig({
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-plain-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                  'sass-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            "@components": path.resolve(
+                __dirname,
+                "resources/assets/js/components"
+            )
+        }
+    }
+})
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css')
